9aca30a19911b49fb63ecaabb404a8ff3fb914e7
GXIIX <51254196+DEVGXIIX@users.noreply.github.com>
2023-06-06 19:28:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, jsonify, request, make_response
 import pandas as pd
 
 # Inicializa o SDK do Firebase com as credenciais baixadas
-cred = credentials.Certificate('credentials.json')
+cred = credentials.Certificate(r'C:\Users\Gianp\OneDrive\Documentos\GitHub\DEF-GATE\credentials.json')
 firebase_admin.initialize_app(cred)
 
 # Cria uma conexão com o Firestore
