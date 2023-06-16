eff107313b82d554f0ac6fad1199ad626145e488
GXIIX <51254196+DEVGXIIX@users.noreply.github.com>
2023-06-16 14:47:07 -0300

    Update Add-Banco.py

--- Add-Banco.py
+++ Add-Banco.py
@@ -1,7 +1,5 @@
 # Bibliotecas usadas
-from flask import Flask, jsonify, request, make_response
 from pymongo import MongoClient
-import pandas as pd
 
 # Criação de uma conexão ao MongoDB localmente
 client = MongoClient("mongodb://localhost:27017/")
@@ -13,7 +11,17 @@ db = client["meu_banco_de_dados"]
 colecao = db["minha_colecao"]
 
 # Dados a serem inseridos
-dados = {"nome": "João", "idade": 30}
+dados = {"Nome": "João Gatilho", 
+         "Idade": 15, 
+         "RG": "546.456.456-89", 
+         "CPF": "489.456.698-78", 
+         "Nome do(a) Responsável": "",
+         "RG do(a) Responsável": "", 
+         "CPF do(a) Responsável": "",
+         "Telefone do Responsável": "", 
+         "E-mail do Responsável": "", 
+         "Endereço": "",
+         "CEP": "",}
 
 # Inserção dos dados
 resultado = colecao.insert_one(dados)
